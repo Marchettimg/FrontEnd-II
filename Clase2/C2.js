e35739fd1277b7c00daa81f2e8e9024b9e2f6cfe
Guillermina <marchettimg@gmail.com>
2022-08-04 17:52:09 -0300

    actualizar

--- Clase2/C2.js
+++ Clase2/C2.js
@@ -20,13 +20,13 @@ let objetoOpciones = {
   let jugar = confirm("¿Querés iniciar la partida?")
 
   //Opcion usuario
-  let opcionUsuario = prompt("Ingrese su opción (piedra, papel, tijera): ")
+  let opcionUsuario = prompt("Ingrese su opción (piedra, papel, tijera): ").toLowerCase()
   console.log(opcionUsuario)
   
  
   // Opcion compu
   let opciones = ["piedra", "papel", "tijera"]
-  let numero = Math.round(Math.random() * (3-1)+1)
+  let numero = Math.round(Math.random() * 3)
   alert("Ahora es el turno de la maquina")
   let opcionCompu = opciones[numero]
   alert(`La opción elegida por la maquina es: ${opcionCompu}`)
